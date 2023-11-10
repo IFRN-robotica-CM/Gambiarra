8fe92a0caa522e70e0a56dcbf0d69fb25e364d60
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2023-11-10 13:40:41 -0300

    adicionando classe para controle de motores

---
+++ Motores.h
@@ -0,0 +1,25 @@
+#ifndef MOTORES_H
+#define MOTORES_H
+
+#include <robo_hardware2.h>
+
+class Motores {
+  
+  private:
+    #define VEL_FRENTE 80
+    #define VEL_TRAS  -60
+
+  public:
+    inline void emFrente(){robo.acionarMotores(VEL_FRENTE, VEL_FRENTE);}
+    inline void  direita(){robo.acionarMotores(VEL_FRENTE,   VEL_TRAS);} 
+    inline void esquerda(){robo.acionarMotores(VEL_TRAS,   VEL_FRENTE);}  
+    inline void paraTras(){robo.acionarMotores(VEL_TRAS,     VEL_TRAS);}
+
+    inline void parar(int tempo){robo.acionarMotores(0, 0); delay(tempo);}
+
+    inline void girar90Dir(){robo.acionarMotores(80, -80); delay(1400);}
+    inline void girar90Esq(){robo.acionarMotores(-80, 80); delay(1400);}
+    
+};
+
+#endif
