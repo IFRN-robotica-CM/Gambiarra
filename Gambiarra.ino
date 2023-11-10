32840a2a8e37173cc376de596763081ac8d87502
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2023-11-10 13:52:28 -0300

    preparando o robô para executar  a estrategia

--- Gambiarra.ino
+++ Gambiarra.ino
@@ -1,7 +1,13 @@
-void setup() {
+#include "Estrategia.h"
+#include <robo_hardware2.h>
 
+Estrategia estrategia;
+
+void setup() {
+  robo.configurar(false);
+  Serial.begin(9600);
 }
 
 void loop() {
-
+  estrategia.executar();
 }
