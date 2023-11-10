a291881e5985152eee4d0e0e136c712f590a166c
Ornã Keivison Fernandes Câmara <132477807+OrnanKeivison@users.noreply.github.com>
2023-11-10 15:44:53 -0300

    adicionando estrategia para desviar de obstáculo

--- Estrategia.h
+++ Estrategia.h
@@ -4,6 +4,7 @@
 #include <robo_hardware2.h>
 #include "Motores.h"
 #include "Refletancia.h"
+#include "Sonar.h"
 
 class Estrategia {
   public:
@@ -13,9 +14,13 @@ class Estrategia {
   private:
 
       void seguirLinha();
+      void alinharLinha();
+      void desviarObstaculoPelaEsq();
+      void desviarObstaculoPelaDir();
 
       Refletancia sensoresLinha;
       Motores motores;
+      Sonar sonar;
 };
 
 #endif
